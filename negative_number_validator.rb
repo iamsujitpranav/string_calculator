457c59b2e36d9df1f643752e1a2de2c18e3938ff
Sujit Pranav <sujitpranavreddy>
2024-07-12 19:24:23 +0530

    added negative number validator logic

--- negative_number_validator.rb
+++ negative_number_validator.rb
@@ -1,2 +1,6 @@
 class NegativeNumberValidator
+  def self.validate(numbers)
+    negatives = numbers.select { |n| n < 0 }
+    raise "Negative numbers not allowed: #{negatives.join(", ")}" unless negatives.empty?
+  end
 end
