e4e4f3c82a29d7b67f38cc264aaeb50cea28da83
Sujit Pranav <sujitpranavreddy>
2024-07-12 19:26:12 +0530

    added no negative numbers test case

--- tests/negative_number_validator.rb_test.rb
+++ tests/negative_number_validator.rb_test.rb
@@ -5,4 +5,8 @@ class NegativeNumberValidatorTest < Minitest::Test
   def test_negative_numbers
     assert_raises(RuntimeError) { NegativeNumberValidator.validate([1, -2, 3]) }
   end
+
+  def test_no_negative_numbers
+    assert_silent { NegativeNumberValidator.validate([1, 2, 3]) }
+  end
 end
