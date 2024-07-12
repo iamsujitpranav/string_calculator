251aa45c03081effcd7abe69003101d009be14d7
Sujit Pranav <sujitpranavreddy>
2024-07-12 19:51:51 +0530

    added invalid string test scenario

--- tests/string_calculator_test.rb
+++ tests/string_calculator_test.rb
@@ -5,4 +5,10 @@ class StringCalculatorTest < Minitest::Test
   def test_add_method_with_empty_string
     assert_equal 0, StringCalculator.add("")
   end
+
+  def test_invalid_input_comma_newline
+    exception = assert_raises(RuntimeError) { StringCalculator.add("1,\n2") }
+    assert_equal "Invalid string", exception.message
+  end
+
 end
