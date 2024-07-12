f512df5ec89bd75a45c8a13aa26ef5bc36e1bcf0
Sujit Pranav <sujitpranavreddy>
2024-07-12 18:09:04 +0530

    re-work

--- negative_number_validator.rb
+++
@@ -1,6 +0,0 @@
-class NegativeNumberValidator
-  def self.validate(numbers)
-    negatives = numbers.select { |n| n < 0 }
-    raise "Negative numbers not allowed: #{negatives.join(", ")}" unless negatives.empty?
-  end
-end
--- tests/string_calculator_test.rb
+++ tests/string_calculator_test.rb
@@ -1,42 +1,8 @@
+require 'minitest/autorun'
 require_relative '../string_calculator'
 
-describe StringCalculator do
-  describe '.add' do
-    context 'when input string is valid' do
-      it 'returns the sum of numbers in the input string' do
-        input = "1,2\n3"
-        expect(StringCalculator.add(input)).to eq(6)
-      end
-    end
-
-    context 'when input string contains negative numbers' do
-      it 'raises an exception' do
-        input = "1,-2\n3"
-        expect { StringCalculator.add(input) }.to raise_error("Negative numbers not allowed: -2")
-      end
-    end
-
-    context 'when input string is invalid' do
-      it 'raises an exception' do
-        input = "1,\n2"
-        expect { StringCalculator.add(input) }.to raise_error("Invalid string")
-      end
-    end
-  end
-
-  describe '.validate_numbers' do
-    context 'when input array contains negative numbers' do
-      it 'raises an exception' do
-        numbers = [1, -2, 3]
-        expect { StringCalculator.validate_numbers(numbers) }.to raise_error("Negative numbers not allowed: -2")
-      end
-    end
-
-    context 'when input array does not contain negative numbers' do
-      it 'does not raise an exception' do
-        numbers = [1, 2, 3]
-        expect { StringCalculator.validate_numbers(numbers) }.not_to raise_error
-      end
-    end
+class StringCalculatorTest < Minitest::Test
+  def test_add_method_with_empty_string
+    assert_equal 0, StringCalculator.add("")
   end
 end
