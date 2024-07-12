b583bffa6ab44b8bc995ce2a5ab94b5b430db20f
Sujit Pranav <sujitpranavreddy>
2024-07-12 19:54:09 +0530

    added negative numbers scenario to string calculator

--- tests/string_calculator_test.rb
+++ tests/string_calculator_test.rb
@@ -11,4 +11,9 @@ class StringCalculatorTest < Minitest::Test
     assert_equal "Invalid string", exception.message
   end
 
+  def test_add_negative_numbers_raises_exception
+    exception = assert_raises(RuntimeError) { StringCalculator.add("1,-2,3,-4") }
+    assert_equal "Negative numbers not allowed: -2, -4", exception.message
+  end
+
 end
